cd85b22bfa9d5aadac0fa3bd76eb6418781e492e
Andre Braga Reis <andre@brg.rs>
2020-04-05 18:45:00 +0100

    Switch to '_or_log' naming

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub trait ResultExt<T, E> {
     ///
     /// Panics if the value is an [`Err`], logging a message provided by the
     /// [`Err`]'s value to a [`slog::Logger`] at a [`Critical`] level.
-    fn unwrap_log(self, log: &slog::Logger) -> T
+    fn unwrap_or_log(self, log: &slog::Logger) -> T
     where
         E: fmt::Debug;
 
@@ -21,7 +21,7 @@ pub trait ResultExt<T, E> {
     ///
     /// Panics if the value is an [`Err`], logging the passed message and the
     /// content of the [`Err`] to a [`slog::Logger`] at a [`Critical`] level.
-    fn expect_log(self, log: &slog::Logger, msg: &str) -> T
+    fn expect_or_log(self, log: &slog::Logger, msg: &str) -> T
     where
         E: fmt::Debug;
 
@@ -31,7 +31,7 @@ pub trait ResultExt<T, E> {
     ///
     /// Panics if the value is an [`Ok`], logging a message provided by the
     /// [`Ok`]'s value to a [`slog::Logger`] at a [`Critical`] level.
-    fn unwrap_err_log(self, log: &slog::Logger) -> E
+    fn unwrap_err_or_log(self, log: &slog::Logger) -> E
     where
         T: fmt::Debug;
 
@@ -41,23 +41,27 @@ pub trait ResultExt<T, E> {
     ///
     /// Panics if the value is an [`Ok`], logging the passed message and the
     /// content of the [`Ok`] to a [`slog::Logger`] at a [`Critical`] level.
-    fn expect_err_log(self, log: &slog::Logger, msg: &str) -> E
+    fn expect_err_or_log(self, log: &slog::Logger, msg: &str) -> E
     where
         T: fmt::Debug;
 }
 
 impl<T, E> ResultExt<T, E> for Result<T, E> {
-    fn unwrap_log(self, log: &slog::Logger) -> T
+    fn unwrap_or_log(self, log: &slog::Logger) -> T
     where
         E: fmt::Debug,
     {
         match self {
             Ok(t) => t,
-            Err(e) => unwrap_failed(log, "called `Result::unwrap_log()` on an `Err` value", &e),
+            Err(e) => unwrap_failed(
+                log,
+                "called `Result::unwrap_or_log()` on an `Err` value",
+                &e,
+            ),
         }
     }
 
-    fn expect_log(self, log: &slog::Logger, msg: &str) -> T
+    fn expect_or_log(self, log: &slog::Logger, msg: &str) -> T
     where
         E: fmt::Debug,
     {
@@ -67,21 +71,21 @@ impl<T, E> ResultExt<T, E> for Result<T, E> {
         }
     }
 
-    fn unwrap_err_log(self, log: &slog::Logger) -> E
+    fn unwrap_err_or_log(self, log: &slog::Logger) -> E
     where
         T: fmt::Debug,
     {
         match self {
             Ok(t) => unwrap_failed(
                 log,
-                "called `Result::unwrap_err_log()` on an `Ok` value",
+                "called `Result::unwrap_err_or_log()` on an `Ok` value",
                 &t,
             ),
             Err(e) => e,
         }
     }
 
-    fn expect_err_log(self, log: &slog::Logger, msg: &str) -> E
+    fn expect_err_or_log(self, log: &slog::Logger, msg: &str) -> E
     where
         T: fmt::Debug,
     {
